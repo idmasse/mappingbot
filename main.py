b4286e7a49972bc7ffcc88a1792bacad0892951b
idmasse <idmasse@icloud.com>
2025-04-26 21:12:09 -0700

    adds main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from map_culture_kings import process_mapping_accept as map_culture_kings
+from map_shopify_brands import process_mapping_accept as map_shopify_brands
+
+def main():
+    map_culture_kings()
+    map_shopify_brands()
+
+if __name__ == '__main__':
+    main()
