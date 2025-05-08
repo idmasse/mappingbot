e1cc7ae3609100acc398881e094198e314aa5ff5
idmasse <idmasse@icloud.com>
2025-05-08 12:40:56 -0700

    adds additional brands to map

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from map_culture_kings import process_mapping_accept as map_culture_kings
+from map_doghugscat import process_mapping_accept as map_doghugscat
+from map_italist import process_mapping_accept as map_italist
+from map_lapopart import process_mapping_accept as map_lapopart
+from map_princesspolly import process_mapping_accept as map_princesspolly
+from map_refinery_no1 import process_mapping_accept as map_refinery_no1
+from map_rustic_marlin import process_mapping_accept as map_rustic_marlin
 from map_shopify_brands import process_mapping_accept as map_shopify_brands
+from map_uniikpillows import process_mapping_accept as map_uniikpillows
 
 def main():
-    map_culture_kings()
-    map_shopify_brands()
+    # map_culture_kings()
+    map_doghugscat()
+    # map_italist()
+    # map_lapopart()
+    # map_princesspolly()
+    # map_refinery_no1()
+    map_rustic_marlin()
+    # map_shopify_brands()
+    map_uniikpillows()
 
 if __name__ == '__main__':
     main()
