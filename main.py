7117892fbc5eb2ea46afa6c0688712ef72ead96b
idmasse <idmasse@icloud.com>
2025-06-20 09:12:53 -0700

    adds additional brands

--- main.py
+++ main.py
@@ -4,24 +4,35 @@ from process_mapping_accept import process_mapping_accept
 def main():
 
     brand_functions = [
-        # get_belt_rhinestone,
+        # get_shopify_connected_brands,
+        # get_italist_brands,
+        # get_culture_kings_brands,
+        # get_princess_polly,
+        # get_refinery_no_1,
+        # get_rustic_marlin,
+        # get_uniikpillows,
+        # get_doghugscat,
+        # get_lapopart,
         # get_liberal_repellent,
-        # get_galaxy_by_harvic,
+        # get_belt_rhinestone,
+        # get_thirdlove,
         # get_harpro,
+        # get_galaxy_by_harvic,
         # get_mothersgold,
         # get_tictoc,
-        # get_thirdlove,
-        # get_lapopart,
-        # get_rustic_marlin,
         # get_petlife,
         # get_lauren_g_adams,
         # get_moonlight_makers,
         # get_bayeas,
+        # get_pipa_fine_art,
         # get_onetify,
         # get_mnml,
-        # get_pipa_fine_art,
-        get_moomaya, #4.4k
-        get_leg_avenue, #1.6k
+        # get_moomaya,
+        # get_leg_avenue,
+        # get_directdeals,
+        # get_anna_kaci,
+        # get_fine_color_jewels,
+        get_fc_design,
     ]
 
     for fn in brand_functions:
